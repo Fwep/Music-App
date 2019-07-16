e01e3e763a03f78cf21a3d1ea9ac4ca823d06e47
Taran Cacacho <tarancacacho@gmail.com>
2019-07-15 19:22:23 -0700

    Write methods to make a new session, login, and logout

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,35 @@
+class SessionsController < ApplicationController
+  protect_from_forgery except: :create
+
+  def new
+    @user = User.new(session_params)
+    render :new
+  end
+  
+  def create
+    @user = User.find_by_credentials(
+      params[:session][:email],
+      params[:session][:password]
+    )
+
+    if @user
+      login_user!(@user)
+      redirect_to user_url(@user)
+    else
+      flash.now[:errors] = "Invalid username/password combination"
+      render :new
+    end
+  end
+
+  def destroy
+    current_user.reset_session_token!
+    session[:session_token] = nil
+
+    redirect_to new_session_url
+  end
+
+  private
+  def session_params
+    params.require(:session).permit(:email, :password)
+  end
+end
