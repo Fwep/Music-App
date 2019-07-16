831accd83f69ec5948c62b0972e374fa4f9db4b4
Taran Cacacho <tarancacacho@gmail.com>
2019-07-15 19:22:02 -0700

    Write users controller logic for making new, creating, and showing a user

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,28 @@
+class UsersController < ApplicationController
+  def new
+    @user = User.new(user_params)
+    render :new
+  end
+
+  def create
+    @user = User.new(user_params)
+
+    if @user.save
+      login_user!(@user)
+      redirect_to users_url
+    else
+      flash.now[:errors] << @user.error.full_messages
+      render :new
+    end
+  end
+
+  def show
+    @user = User.find(params[:id])
+    render :show
+  end
+
+  private
+  def user_params
+    params.require(:user).permit(:email, :password)
+  end
+end
