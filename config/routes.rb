a9ceec316286da75a705d7d768527731756b6166
Taran Cacacho <tarancacacho@gmail.com>
2019-07-17 10:58:32 -0700

    Fix typo

--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,9 @@ Rails.application.routes.draw do
     resources :albums, only: :new
   end
 
-  resources :albums, except: :new
+  resources :albums, except: :new do
+    resources :tracks, only: [:new]
+  end
 
-  resources :tracks
-end
+  resources :tracks, except: :new
+ end
